a85357a727656f70325efcbef6682201945fdf52
Shaswatha Thilaka <83292249+shash1110@users.noreply.github.com>
2024-04-12 06:46:26 +0530

    Time: 6 ms (12.13%) | Memory: 45.4 MB (93.16%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,25 +1,19 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        if(n<=0){
+        if(height==null)
             return 0;
+        Stack<Integer> s = new Stack<>();
+        int i = 0, max =0, maxbot =0;
+        while(i< height.length){
+            if(s.isEmpty() || height[i]<=height[s.peek()]){
+                s.push(i++);
+            }
+            else{
+                int bot = s.pop();
+                maxbot = s.isEmpty() ? 0 : (Math.min (height[s.peek()], height[i])-height[bot])*(i-s.peek()-1);
+                max += maxbot;
+            }
         }
-        int [] left = new int[n];
-        int [] right = new int[n];
-        left[0] = height[0];
-        right[n-1] = height[n-1];
-        for(int i=1;i<n;i++){
-            left[i] = Math.max(left[i-1],height[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            right[i] = Math.max(right[i+1],height[i]);
-        }
-        int trap=0;
-        for(int i=0;i<n;i++){
-            int minheight = Math.min(left[i], right[i]);
-            trap += Math.max(0, minheight - height[i]);
-        }
-        return trap;
-
+        return max;
     }
 }
