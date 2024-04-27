198b1c8144ed5a8193aebe3334fe4505832f6daf
Shaswatha Thilaka <83292249+shash1110@users.noreply.github.com>
2024-04-27 08:06:55 +0530

    Time: 5 ms (100.00%) | Memory: 45.4 MB (9.71%) - LeetSync

--- 514-freedom-trail/freedom-trail.java
+++ 514-freedom-trail/freedom-trail.java
@@ -1,25 +1,27 @@
 class Solution {
-  public int findRotateSteps(String ring, String key) {
-    Map<String, Integer> mem = new HashMap<>();
-    return dfs(ring, key, 0, mem) + key.length();
-  }
-  private int dfs(final String ring, final String key, int index, Map<String, Integer> mem) {
-        if (index == key.length())
-        return 0;
-        final String hashKey = ring + index;
-        if (mem.containsKey(hashKey))
-        return mem.get(hashKey);
-        
-        int ans = Integer.MAX_VALUE;
-        for (int i = 0; i < ring.length(); ++i)
-        if (ring.charAt(i) == key.charAt(index)) {
-            final int minRotates = Math.min(i, ring.length() - i);
-            final String newRing = ring.substring(i) + ring.substring(0, i);
-            final int remainingRotates = dfs(newRing, key, index + 1, mem);
-            ans = Math.min(ans, minRotates + remainingRotates);
+    public int findRotateSteps(String ring, String key) {
+        char[] r = ring.toCharArray();
+        List<Integer>[] positions = new List[26];
+        for (int i = 0; i < r.length; i++) {
+            int c = r[i] - 'a';
+            if (positions[c] == null) positions[c] = new ArrayList<>();
+            positions[c].add(i);
         }
+        int[][] dp = new int[key.length()][r.length];
+        return helper(0, 0, positions, key.toCharArray(), r, dp);
+    }
+    int helper(int index, int pos, List<Integer>[] positions, char[] key, char[] ring, int[][] dp) {
 
-        mem.put(hashKey, ans);
-        return ans;
-  }
+        if (index == key.length) return 0;
+        if (dp[index][pos] > 0) return dp[index][pos];
+        char target = key[index];
+        List<Integer> possiblePositions = positions[target - 'a'];
+        int minSteps = Integer.MAX_VALUE;
+        for (int nextPos : possiblePositions) {
+           int steps = Math.min(Math.abs(nextPos - pos), ring.length - Math.abs(nextPos - pos));
+            int totalSteps = steps + helper(index + 1, nextPos, positions, key, ring, dp);
+            minSteps = Math.min(minSteps, totalSteps);
+        }
+        return dp[index][pos] = minSteps + 1;
+    }
 }
