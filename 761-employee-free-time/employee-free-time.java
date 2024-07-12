a4fd1e81db9fd40aae346a3b0a80048c054ce2b3
Shaswatha Thilaka <83292249+shash1110@users.noreply.github.com>
2024-07-12 06:28:03 +0530

    Time: 16 ms (29.92%) | Memory: 45.9 MB (48.78%) - LeetSync

---
+++ 761-employee-free-time/employee-free-time.java
@@ -0,0 +1,37 @@
+/*
+// Definition for an Interval.
+class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {}
+
+    public Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> schedule.get(a[0]).get(a[1]).start - schedule.get(b[0]).get(b[1]).start);
+        for (int i = 0; i < schedule.size(); i++) {
+            pq.add(new int[] {i, 0});
+        }
+        List<Interval> res = new ArrayList<>();
+        int prev = schedule.get(pq.peek()[0]).get(pq.peek()[1]).start;
+        while (!pq.isEmpty()) {
+            int[] index = pq.poll();
+            Interval interval = schedule.get(index[0]).get(index[1]);
+            if (interval.start > prev) {
+                res.add(new Interval(prev, interval.start));
+            }
+            prev = Math.max(prev, interval.end);
+            if (schedule.get(index[0]).size() > index[1] + 1) {
+                pq.add(new int[] {index[0], index[1] + 1});
+            }
+        }
+        return res;
+    }
+}
