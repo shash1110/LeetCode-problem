d33b09ec205e3f1045e190b8bab02a37f24c5866
Shaswatha Thilaka <83292249+shash1110@users.noreply.github.com>
2024-08-14 07:59:31 +0530

    Time: 8 ms (81.12%) | Memory: 44.5 MB (40.48%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -1,32 +1,30 @@
 class Solution {
-    private int countPairs(int[] a, int mid) {
-        int n = a.length, res = 0;
-        for (int i = 0; i < n; ++i) {
-            int j = i;
-            while (j < n && a[j] - a[i] <= mid) j++;
-            res += j - i - 1;
+    public int smallestDistancePair(int[] nums, int k) {
+       Arrays.sort(nums);
+       int low = 0;
+       int high = nums[nums.length-1]-nums[0];
+       while(low<high){
+        int mid = low +(high-low)/2;
+        int count = countPairsWithSumMid(nums,mid);
+        if(count>=k){
+          high=mid;   
+        } else {
+            low = mid+1;
         }
-        return res;
+       }
+       return low;    
     }
+    int countPairsWithSumMid(int[] nums, int sum){
+        int count=0;
+        int left=0;
 
-    public int smallestDistancePair(int a[], int k) {
-        int n = a.length;
-        Arrays.sort(a);
-
-        int low = a[1] - a[0];
-        for (int i = 1; i < n - 1; i++)
-            low = Math.min(low, a[i + 1] - a[i]);
-
-        int high = a[n - 1] - a[0];
-
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (countPairs(a, mid) < k)
-                low = mid + 1;
-            else
-                high = mid;
+        for(int right=1;right<nums.length;right++){
+            while(nums[right]-nums[left]>sum){
+                left++;
+            }
+            count+=right-left;  
         }
-
-        return low;
+        return count;
+        
     }
 }
